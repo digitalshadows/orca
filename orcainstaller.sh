c5d8bd82de023b4f90031fada49702377c0e6875
charles-ragland <53487181+charles-ragland@users.noreply.github.com>
2020-06-15 10:16:12 -0500

    fixed the directory errors

--- orcainstaller.sh
+++ orcainstaller.sh
@@ -4,11 +4,10 @@
 ###only tested on a fresh Ubuntu 18.04 LTS install
 ###add shodan api key as an environmental variable prior to running, or just replace it in the script
 
+SHODAN_API_KEY=your_key_here
+
 echo 'Updating packages...'
 sudo apt update -y
-
-echo 'Upgrading packages...'
-sudo apt full-upgrade -y
 
 echo 'Installing packages...'
 sudo apt install apt-transport-https ca-certificates curl software-properties-common build-essential python3-pip -y
@@ -21,7 +20,6 @@ sudo systemctl enable docker
 
 echo 'Installing Orca...'
 git clone https://github.com/digitalshadows/orca.git
-cd orca
 sudo docker-compose up -d
 sudo -H pip3 install .
 orca-recon init $SHODAN_API_KEY
@@ -29,9 +27,5 @@ echo "$(_ORCA_RECON_COMPLETE=source orca-recon)" > ~/.orca/orca-recon-complete.s
 echo '. ~/.orca/orca-recon-complete.sh' >> ~/.bashrc
 cd ..
 source ~/.bashrc
-
-echo 'Cleaning up...'
-sudo apt autoclean -y
-sudo apt autoremove -y
 
 echo 'Finished!'
